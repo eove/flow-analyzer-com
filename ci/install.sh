e9738571a852d05820a8d13eecae33fef24cd514
Peio Roth <pierreroth64@gmail.com>
2020-09-28 15:04:19 +0200

    update CI scripts

---
+++ ci/install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+main() {
+  local secret="$1"
+  prepare_authentication "${secret}"
+  npm install --no-progress
+  npm run bootstrap
+}
+
+prepare_authentication() {
+  local secret="$1"
+  prepare_directory . "${secret}"
+  for directory in packages/*/ ; do
+    prepare_directory "${directory}" "${secret}"
+  done
+}
+
+prepare_directory() {
+  local directory="$1"
+  local secret="$2"
+  echo "Preparing authentication in ${directory}"
+  echo "//npm.pkg.github.com/:_authToken=${secret}" > "${directory}/.npmrc"
+  echo "registry=https://npm.pkg.github.com/eove" >> "${directory}/.npmrc"
+  sed -i "s/ssh:\/\/git@github.com/https:\/\/${secret}:x-oauth-basic@github.com/g" "${directory}/package.json"
+  sed -i "s/ssh:\/\/git@github.com/https:\/\/${secret}:x-oauth-basic@github.com/g" "${directory}/package-lock.json"
+}
+
+main "$@"
