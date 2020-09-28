85bae272c2944beb67ad827ed277f47f8c88a3ca
Peio Roth <pierreroth64@gmail.com>
2020-09-28 21:19:58 +0200

    add console logs to install script

--- ci/install.sh
+++ ci/install.sh
@@ -5,14 +5,18 @@ set -e
 main() {
   local secret="$1"
   prepare_authentication "${secret}"
+  echo "Installing root packages"
   npm install --no-progress
+  echo "Bootstraping"
   npm run bootstrap
 }
 
 prepare_authentication() {
   local secret="$1"
+  echo "Preparing authentication in ."
   prepare_directory . "${secret}"
   for directory in packages/*/ ; do
+    echo "Preparing authentication in ${directory}"
     prepare_directory "${directory}" "${secret}"
   done
 }
@@ -20,7 +24,6 @@ prepare_authentication() {
 prepare_directory() {
   local directory="$1"
   local secret="$2"
-  echo "Preparing authentication in ${directory}"
   echo "//npm.pkg.github.com/:_authToken=${secret}" > "${directory}/.npmrc"
   echo "registry=https://npm.pkg.github.com/eove" >> "${directory}/.npmrc"
   sed -i "s/ssh:\/\/git@github.com/https:\/\/${secret}:x-oauth-basic@github.com/g" "${directory}/package.json"
