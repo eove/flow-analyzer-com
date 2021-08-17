b3c9fe6182b834f1799fda331b16b8eb70f2ac70
Peio Roth <pierreroth64@gmail.com>
2021-08-17 10:53:04 +0200

    update install script

--- ci/install.sh
+++ ci/install.sh
@@ -3,26 +3,10 @@
 set -e
 
 main() {
-  local secret="$1"
-  prepare_authentication "${secret}"
+  echo "Installing root packages"
   npm install --no-progress
+  echo "Bootstraping"
   npm run bootstrap
-}
-
-prepare_authentication() {
-  local secret="$1"
-  prepare_directory . "${secret}"
-  for directory in packages/*/ ; do
-    prepare_directory "${directory}" "${secret}"
-  done
-}
-
-prepare_directory() {
-  local directory="$1"
-  local secret="$2"
-  echo "Preparing authentication in ${directory}"
-  echo "//npm.pkg.github.com/:_authToken=${secret}" > "${directory}/.npmrc"
-  echo "registry=https://npm.pkg.github.com/eove" >> "${directory}/.npmrc"
 }
 
 main "$@"
